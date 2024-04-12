067fb0a1e6e2bd22e29ad73018c5c0e76ae7f892
Paulo <git@pauloo27.dev>
2024-04-11 23:27:52 -0300

    feat: cors

--- src/main.js
+++ src/main.js
@@ -12,6 +12,13 @@ async function main() {
 
   const app = express();
 
+  app.use((_req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
+
   app.use(express.json());
 
   route(app);
